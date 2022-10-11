726f2641cb11cccb7e353a844a2e2f3925adc16e
Olive Power <powerm@google.com>
2022-10-11 15:30:27 +0200

    changed the name

--- run/helloworld/Program.cs
+++ run/helloworld/Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 var url = $"http://0.0.0.0:{port}";
-var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
+var target = Environment.GetEnvironmentVariable("TARGET") ?? "Olive";
 
 var app = builder.Build();
 
