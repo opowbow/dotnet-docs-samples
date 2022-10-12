52e159c3172cca4fe8732829f4661c3fa76c5ed9
Olive Power <powerm@google.com>
2022-10-12 19:06:06 +0200

    new target

--- run/helloworld/Program.cs
+++ run/helloworld/Program.cs
@@ -17,7 +17,6 @@ var builder = WebApplication.CreateBuilder(args);
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 var url = $"http://0.0.0.0:{port}";
 var target = Environment.GetEnvironmentVariable("TARGET") ?? "Olive Power";
-
 var app = builder.Build();
 
 app.MapGet("/", () => $"Hello {target}!");
